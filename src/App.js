a9dfe7fee407c8051c15edf83ddb5b953b51287b
Jacek Zielinski <jacekzielinski0787@gmail.com>
2023-09-27 18:11:19 +0200

    Simple Profile Card

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import "./style.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="card">
+      <Avatar />
+      <div className="data">
+        <Intro />
+
+        <SkillList />
+      </div>
+    </div>
+  );
+}
+
+function Avatar() {
+  return <img className="avatar" src="./gandi.jpg" alt="My pic"></img>;
+}
+
+function Intro() {
+  return (
+    <div>
+      <h1>Jacek Zielinski</h1>
+      <p>
+        Junior Front-end Developer. Coding is my big passion.I love to create
+        something new.<br></br>I like my family, video games and good music.
+      </p>
+    </div>
+  );
+}
+
+function SkillList() {
+  return (
+    <div className="skill-list">
+      <Skill skill="React" emoji=" 💪" color="#6756DA" />
+      <Skill skill="Html + Css" emoji=" 💪" color="#E1BE47" />
+      <Skill skill="JavaScript" emoji=" 💪" color="#FFCD94" />
+      <Skill skill="Git + Github" emoji=" 💪" color="#900808" />
+    </div>
+  );
+}
+
+function Skill(props) {
+  return (
+    <div className="skill" style={{ backgroundColor: props.color }}>
+      <span>{props.skill}</span>
+      <span>{props.emoji}</span>
     </div>
   );
 }
